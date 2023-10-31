ec9d2e437eae4d940ffbe393ca4cb04e4aa407cd
Finn Jung <urush030219@gmail.com>
2023-10-31 14:45:57 -0600

    minor changes

--- cvpractice1.py
+++ cvpractice1.py
@@ -1,8 +1,17 @@
 """
 1. cv2.imread(r"filepath") 
     a. read datas of the file
-
-
+2. cv2.split(image)
+    a. splitting the image into images composed only of r, g, and b values
+3. cv2.merge(r, g, b)
+    a. opposite of split, takes in however many arguments
+4. cv2.cvtColor(inputImage, conversionCode)
+    a. convert colors from RGB to BRG, etc
+        i. cv2.COLOR_BGR2RGB, cv2.COLOR_BGR2HSV, etc
+    b. OpenCV retrieves images as BRG instead of RGB
+    c. to merge, the three values must be in r, g, and b, not other format: another conversino required
+5. cv2.imwrite(filename, img[parameters])
+    a. storing newly composed images
 
 """
 import os
@@ -16,15 +25,17 @@ from urllib.request import urlretrieve
 address = r"C:\Users\OWNER\Pictures\Lightroom Saved Photos\Sangju 05_22_2023\Places\DSC07897.jpg"
 
 img = cv2.imread(address)
-print(cv2.split(img))
+newImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+h, s, v = cv2.split(newImg)
+plt.imshow(img)
+plt.waitforbuttonpress()
 
-"""plt.figure(figsize=[10,5])
+plt.figure(figsize=[20,5])
 
-plt.subplot();plt.imshow(r, cmap="gray");plt.title("Red")
-plt.subplot(142);plt.imshow(g, cmap="gray");plt.title("Green")
-plt.subplot(143);plt.imshow(b, cmap="gray");plt.title("Blue")
-
-
-imgMerged = cv2.merge((b,g,r))
-plt.subplot(144);plt.imshow(imgMerged[:,:,::-1]);plt.title("Merged")
-plt.waitforbuttonpress()"""
+plt.subplot(151);plt.imshow(h, cmap="gray");plt.title("hue")
+plt.subplot(152);plt.imshow(s, cmap="gray");plt.title("saturation")
+plt.subplot(153);plt.imshow(v, cmap="gray");plt.title("value")
+plt.subplot(154);plt.imshow(img);plt.title("original image")
+imgMerged = cv2.merge((h,s,v))
+plt.subplot(155);plt.imshow(imgMerged);plt.title("Merged")
+plt.waitforbuttonpress()
